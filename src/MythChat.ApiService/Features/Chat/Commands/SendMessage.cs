be9d8fb0a6c4ef52fcee8b47f85844008db44083
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2023-11-30 04:41:59 -0400

    Add SendMessage Chat command

---
+++ src/MythChat.ApiService/Features/Chat/Commands/SendMessage.cs
@@ -0,0 +1,51 @@
+﻿using Carter;
+using Carter.ModelBinding;
+
+using FluentValidation;
+
+using MediatR;
+
+namespace MythChat.ApiService.Features.Chat.Commands;
+
+public class SendMessage : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/chat", async (SendMessageCommand command, IMediator mediator) => await mediator.Send(command))
+            .WithName(nameof(SendMessage))
+            .WithTags("Chat")
+            .ProducesValidationProblem()
+            .Produces<IResult>(StatusCodes.Status200OK)
+            .WithOpenApi();
+    }
+
+    public class SendMessageCommand : IRequest<IResult>
+    {
+        public string? Agent { get; set; }
+        public string? Channel { get; set; }
+        public string? Message { get; set; }
+    }
+
+    public class SendMessageCommandHandler(
+        IValidator<SendMessageCommand> validator) : IRequestHandler<SendMessageCommand, IResult>
+    {
+        public async Task<IResult> Handle(SendMessageCommand request, CancellationToken cancellationToken)
+        {
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            return !validationResult.IsValid
+                ? Results.ValidationProblem(validationResult.GetValidationProblems())
+                : throw new NotImplementedException();
+        }
+    }
+
+    public class SendMessageCommandValidator : AbstractValidator<SendMessageCommand>
+    {
+        public SendMessageCommandValidator()
+        {
+            RuleFor(x => x.Agent).NotEmpty();
+            RuleFor(x => x.Channel).NotEmpty();
+            RuleFor(x => x.Message).NotEmpty();
+        }
+    }
+}
--- src/MythChat.ApiService/Program.cs
+++ src/MythChat.ApiService/Program.cs
@@ -1,39 +1,32 @@
+using Carter;
+
+using FluentValidation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire components.
 builder.AddServiceDefaults();
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddProblemDetails();
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<Program>());
+builder.Services.AddValidatorsFromAssemblyContaining(typeof(Program));
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.UseStaticFiles();
 
 app.MapDefaultEndpoints();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
