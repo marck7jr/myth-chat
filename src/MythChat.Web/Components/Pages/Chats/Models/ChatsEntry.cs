2d74ebf418974ab132ba1e599146c4c1a0f89930
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2023-12-01 23:07:42 -0400

    Add Chats page

---
+++ src/MythChat.Web/Components/Pages/Chats/Models/ChatsEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace MythChat.Web.Components.Pages.Chats.Models;
+
+public class ChatsEntry
+{
+    public bool IsFromAgent { get; set; }
+    public string? Author { get; set; }
+    public string? Content { get; set; }
+}
